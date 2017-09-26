6e7eb4f9d9d726cc4e3903a9b3d30f5f2aa21735
tfs <526689497@qq.com>
2017-09-26 20:41:37 +0800

    vue商城部分功能的实现

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<!-- 以后项目的根组件 -->
+<template>
+	<div>
+		<!-- 1.0 利用mint-ui中的header组件四线整个系统的头部 -->
+		<mt-header fixed title="Vue项目商场"></mt-header>
+		<!-- 2.0 利用vue-route的<router-view>进行占位 -->
+		<router-view></router-view>
+		<!-- 3.0 利用mui中的tabbar组件实现系统的地步 -->
+		<nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"></span>
+				<span class="mui-tab-label">消息</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">通讯录</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</router-link>
+		</nav>
+	</div>
+</template>
+<script>
+	// 负责导出 .vue这个组件对象(它本质上是一个Vue对象,所以Vue中该定义的元素都可以使用)
+	export default{  // es6的导出对象的写法
+		data(){  //等价于 es5的 data:function(){
+			return {}
+		},
+		methods:{},
+		created(){}
+	}
+</script>
+<style scoped>
+</style>
